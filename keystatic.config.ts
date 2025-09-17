9d8903594754196b630d1e3bc5542288a1c6c142
Caden Holliday <cadenholliday23@gmail.com>
2025-09-17 11:47:07 -0700

    keystatic.config.ts

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -6,7 +6,7 @@ export default config({
     kind: "cloud",
   },
   cloud: {
-    project: "cadenholliday23-ux/gateway-fellowship",
+    project: "gateway-fellowship/gateway-fellowship",
   },
   collections: {
     pages: collection({

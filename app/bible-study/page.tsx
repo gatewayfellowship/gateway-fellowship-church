65c524ac5920fc4f8b6f9b1c0b94deb8dad79108
gatewayfellowship <gatewayfellowship1943@gmail.com>
2025-09-18 14:56:49 -0700

    Create page.tsx

---
+++ app/bible-study/page.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Markdoc from "@markdoc/markdoc";
+import reader from "../../keystatic/reader"; // adjust path relative to page
+import { Jumbotron } from "../../components/Jumbotron";
+import { ContentContainer } from "../../components/ContentContainer";
+
+export default async function BibleStudy() {
+  const page = await reader.collections.pages.read("bible-study");
+
+  if (!page) {
+    return "LOADING...";
+  }
+
+  const { node } = await page.content();
+  const renderable = Markdoc.transform(node);
+
+  return (
+    <main>
+      <Jumbotron title={page.title} imageSrc={page.imageSrc} />
+      <ContentContainer>
+        {Markdoc.renderers.react(renderable, React)}
+      </ContentContainer>
+    </main>
+  );
+}
